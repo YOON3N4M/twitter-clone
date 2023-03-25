3d6d9127c703811460db0efb0296aefbabeb906f
yoon3nam <lgmysn2@gmail.com>
2023-03-25 13:17:07 +0900

    스타일 적용 전 백업

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,17 +6,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { setSignIn, setSignOut, setFBaseInit, setUser } from "../modules/store";
 import Navigation from "./Navigation";
 import { updateProfile } from "@firebase/auth";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 type UserT = null | Object;
 
 function App() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         dispatch(setSignIn());
         dispatch(setUser(user));
+        navigate("/home");
       } else {
         dispatch(setSignOut());
         dispatch(setUser({}));
@@ -32,5 +36,9 @@ function App() {
     </>
   );
 }
+
+const styledContainer = styled.div`
+  width: 100%;
+`;
 
 export default App;
